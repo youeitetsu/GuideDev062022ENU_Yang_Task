1f845308b7b8378cf9db92d474da2d5a2684c18e
{youeitetsu} <{ray.1212@163.com}>
2022-07-04 00:43:53 +0900

    Visit detail, something about calculate Commission and Validator added

---
+++ GuideDev062022ENU_Yang/Schemas/UsrRealtyVisitPage/UsrRealtyVisitPage.js
@@ -0,0 +1,68 @@
+define("UsrRealtyVisitPage", [], function() {
+	return {
+		entitySchemaName: "UsrRealtyVisit",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "LOOKUPf8bf7387-373f-4c38-a015-c6cffd83d47f",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrPotentialCustomer",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "UsrVisitDatetime0599ab39-db9f-4f6e-8c5c-0c8d95f5ef01",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrVisitDatetime"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGd61374e9-57f7-4460-b9d6-2c768ed178b6",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrComment",
+					"enabled": true,
+					"contentType": 0
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
