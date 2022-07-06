d83146393504d0d512a3e1d8d8aedaf8b5368963
{youeitetsu} <{ray.1212@163.com}>
2022-07-07 00:28:20 +0900

    modified "Calc average price process" and "Add realty visit and refresh detail"

---
+++ GuideDev062022ENU_Yang/Schemas/UsrDecimalOutputPage/UsrDecimalOutputPage.js
@@ -0,0 +1,31 @@
+define("UsrDecimalOutputPage", [], function() {
+	return {
+		entitySchemaName: "",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "FLOATd3df92a2-4b75-43ac-bf74-465cc4a89723",
+				"values": {
+					"layout": {
+						"colSpan": 8,
+						"rowSpan": 1,
+						"column": 8,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrResultUSDSqM",
+					"enabled": false
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
